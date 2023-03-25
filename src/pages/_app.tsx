5f8c6b2a02870f76fbaccc869d3f48021eec2e75
Dominik Sieroń <71605737+CALLmeDOMIN@users.noreply.github.com>
2023-03-25 16:14:16 +0100

    added fontello

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,5 @@
 import { type AppType } from "next/dist/shared/lib/utils";
-
+import "../../fontello/css/fontello.css";
 import "@/styles/globals.css";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
