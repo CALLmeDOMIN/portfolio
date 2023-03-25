5d0b3f61fdc41965cb8c955bfadacd73a2f4ae9a
Dominik Sieroń <71605737+CALLmeDOMIN@users.noreply.github.com>
2023-03-25 16:51:54 +0100

    fixed bugs

--- src/pages/nav.tsx
+++ src/pages/nav.tsx
@@ -1,9 +1,9 @@
 import Image from "next/image"
 import Link from "next/link"
 
-export default function Nav(...props:any) {
+export default function Nav({id}:{id:string}) {
 	return (
-		<nav {...props.id} className="w-screen flex items-center justify-between gap-8 p-4 px-10 h-1/3 bg-slate-900">
+		<nav id={id} className="w-screen flex items-center justify-between gap-8 p-4 px-10 h-1/3 bg-slate-900">
 			<Link href={'#home'}><Image src={"/logo.png"} alt="Logo" width={48} height={48}></Image></Link >
 			<ul className="p-0 m-0 text-gray-300 font-Poppins font-bold list-none flex gap-4">
 				<li ><Link className="flex items-strech justify-center p-1 hover:text-pink-400 transition-all duration-500" href={'#about'}>About me</Link></li>
