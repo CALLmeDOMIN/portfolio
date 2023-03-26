a2e8e42b178123a829cc202272878734a7382214
Dominik Sieroń <71605737+CALLmeDOMIN@users.noreply.github.com>
2023-03-26 18:47:32 +0200

    aboutme section

--- src/pages/nav.tsx
+++ src/pages/nav.tsx
@@ -7,14 +7,14 @@ export default function Nav({ id }: { id: string }) {
 
 
 	return (
-		<nav id={id} className="w-screen flex items-center justify-between gap-8 p-4 md:px-10 h-1/3 bg-slate-900">
-			<Link href={'#home'}><Image src={"/logo.png"} alt="Logo" width={48} height={48}></Image></Link >
-			<div onClick={() => setOpen(!open)} className="md:hidden h-full z-20 aspect-square text-3xl text-pink-400 cursor-pointer">
+		<nav id={id} className="w-screen flex fixed top-0  items-center justify-between gap-8 p-8 md:px-10 shadow-xl opacity-90 bg-slate-900">
+			<Link className="opacity-100" href={'#home'}><Image src={"/logo.png"} alt="Logo" width={48} height={48}></Image></Link >
+			<div onClick={() => setOpen(!open)} className="transition-all duration-500 md:hidden h-full z-20 aspect-square text-3xl text-pink-400 cursor-pointer">
 				<i className={open ? 'icon-cancel' : 'icon-menu'} />
 			</div>
 			<ul className={`pt-12 md:p-0 py-10 mt-0 text-gray-300
 			 font-Poppins font-bold list-none flex flex-col md:flex-row md:static z-10
-			 md:z-auto absolute shadow-2xl md:shadow-none w-3/4 h-screen md:w-auto
+			 md:z-auto fixed shadow-2xl md:shadow-none w-3/4 h-screen md:w-auto
 			 md:h-auto top-0 gap-10 md:gap-4 bg-inherit md:bg-transparent right-0
 			 opacity-0 md:opacity-100 transition-all ease-in duration-500 ${open ? 'opacity-100' : 'opacity-0'}`}>
 				<li ><Link onClick={() => setOpen(false)} className="flex items-strech justify-center p-1 hover:text-pink-400 transition-all duration-500" href={'#about'}>About me</Link></li>
@@ -23,7 +23,7 @@ export default function Nav({ id }: { id: string }) {
 				<li><Link onClick={() => setOpen(false)} className="flex items-center justify-center p-1 hover:text-pink-400 transition-all duration-500" href={'#resume'}>Resume</Link></li>
 			</ul>
 			<div className={`opacity-0 md:hidden backdrop-blur-sm 
-			md:backdrop-blur-none bg-transparent absolute z-10 w-1/4 h-screen 
+			md:backdrop-blur-none bg-transparent fixed z-[5] w-screen h-screen 
 			top-0 transition-all ease-in duration-500 ${open ? 'opacity-100' : 'opacity-0'}
 			`}></div>
 		</nav >
