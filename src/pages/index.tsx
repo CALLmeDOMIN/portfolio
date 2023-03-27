e3b282b206db0ae9b769a117dd76b1e78583b84c
Dominik Sieroń <71605737+CALLmeDOMIN@users.noreply.github.com>
2023-03-27 20:26:41 +0200

    fixed animations and blur

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -31,8 +31,8 @@ const Home: NextPage = () => {
               <h2 className="text-3xl text-gray-300 font-extralight">Hi, my name is</h2>
               <h1 className="-ml-1 text-7xl text-[#ff6ec7] font-semibold">Dominik Sieroń.</h1>
               <p className="text-gray-300 text-xl mt-4 mx-4">
-                Software engineer with a passion for <span className="text-pink-400">web development</span>. <br />
-                I am currently studying Computer Science in Engineering at <br /><span className="text-pink-400">AGH University of Science and Technology</span>.
+                I am software engineer with a passion for <span className="text-pink-400">web development</span>. <br />
+                Currently studying Computer Science in Engineering at <br /><span className="text-pink-400">AGH University of Science and Technology</span>.
               </p>
               <button className="sm:w-1/3 min-w-min mt-8 py-4 px-8 bg-transparent text-xl font-semibold text-pink-400 border border-pink-400 rounded-xl transition-all duration-500 hover:bg-pink-400/20">Check out my CV!</button>
             </section>
