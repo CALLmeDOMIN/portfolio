13bfcccf51e8adc043b5b10a83060434bb74405a
Dominik Sieroń <71605737+CALLmeDOMIN@users.noreply.github.com>
2023-05-08 20:36:16 +0200

    added certification

--- src/pages/certificates.tsx
+++ src/pages/certificates.tsx
@@ -21,20 +21,42 @@ export default function Certificates() {
       >
         <div className="flex">
           <IconCertificate className="w-12 h-12 text-gray-300 mr-2" />
-          <h2 className="text-5xl font-bold mb-4 text-gray-300">Certificates</h2>
+          <h2 className="text-5xl font-bold mb-4 text-gray-300">
+            Certificates
+          </h2>
         </div>
         <div className="md:w-5/6 text-justify pl-2 border-l border-pink-400 hover:border-white">
           <p className=" md:text-xl font-light transition-all duration-500 text-gray-300 mb-4">
             JavaScript Algorithms and Data Structures by freeCodeCamp.org
           </p>
-		  <p className=" md:text-md font-light transition-all duration-500 text-gray-300 mb-4 pl-4">
-			Developer Certification, representing approximately 300 hours of coursework.
-		  </p>
+          <p className=" md:text-md font-light transition-all duration-500 text-gray-300 mb-4 pl-4">
+            Developer Certification, representing approximately 300 hours of
+            coursework.
+          </p>
           <Link
             target="_blank"
             className="text-pink-400 pl-4"
             href={
               "https://www.freecodecamp.org/certification/CALLmeDOMIN/javascript-algorithms-and-data-structures"
+            }
+          >
+            Look up
+          </Link>
+        </div>
+
+        <div className="md:w-5/6 mt-4 text-justify pl-2 border-l border-pink-400 hover:border-white">
+          <p className=" md:text-xl font-light transition-all duration-500 text-gray-300 mb-4">
+            SQL Essential Training by Linkedin
+          </p>
+          <p className=" md:text-md font-light transition-all duration-500 text-gray-300 mb-4 pl-4">
+            Developer Certification, representing knowledge of SQL/SQLite
+            language.
+          </p>
+          <Link
+            target="_blank"
+            className="text-pink-400 pl-4"
+            href={
+              "https://www.linkedin.com/learning/certificates/d9ee88b0d34eaafa1c30d8a5b86aa07bfbf73956f5c1cfa8ac397fcc70f7f317?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base%3BPXoqZb2MSw6qBkVaX6Qnrg%3D%3D"
             }
           >
             Look up
