76ae9cc03c08c4df961876fda1d7a4e034e08cc8
Dominik Sieroń <dominiksieron13@gmail.com>
2024-02-23 18:27:30 +0100

    Revert "Fix: Weird behaviour gix"
    This reverts commit aff8fd46d71567fa4f648eedb932438f65e03c3f.

---
+++ src/app/frontendmentor/page.tsx
@@ -0,0 +1,71 @@
+import FrontendMentor from "@/components/Frontendmentor";
+import Link from "next/link";
+import { IconArrowUpRight } from "@tabler/icons-react";
+
+const Mentor = () => {
+    return (
+        <>
+            <div className="flex min-h-screen flex-col items-center justify-center">
+                <h2 className="grid grid-cols-4 py-4 text-3xl font-semibold text-text">
+                    <Link href={"/"} className="my-12 pr-10">
+                        <button>&larr; Back</button>
+                    </Link>
+                    <div className="col-span-3 flex items-center">
+                        <h1>Frontend Mentor</h1>
+                        <Link
+                            href={"https://www.frontendmentor.io/"}
+                            target="about:blank"
+                            className="pl-2 text-accent"
+                        >
+                            <IconArrowUpRight />
+                        </Link>
+                    </div>
+                </h2>
+                <div className="">
+                    <div className="mb-4 grid max-w-7xl gap-4 px-4 transition-all duration-1000 odd:delay-300 even:delay-700 sm:grid-cols-2 lg:grid-cols-3">
+                        <FrontendMentor
+                            image="/advice.jpg"
+                            alt="advice generator"
+                            github="https://github.com/CALLmeDOMIN/advice-generator"
+                            vercel="https://advice-generator-gold-one.vercel.app/"
+                            whiteText
+                        />
+                        <FrontendMentor
+                            image="/newsletter.jpg"
+                            alt="newsletter sign up"
+                            github="https://github.com/CALLmeDOMIN/newsletter-sign-up"
+                            vercel="https://newsletter-sign-up-delta.vercel.app/"
+                            whiteText
+                        />
+                        <FrontendMentor
+                            image="/tipcalc.jpg"
+                            alt="tip calculator"
+                            github="https://github.com/CALLmeDOMIN/tip-calculator"
+                            vercel="https://tip-calculator-dominiksieron.vercel.app/"
+                        />
+                        <FrontendMentor
+                            image="/summarycomponent.png"
+                            alt="summary component"
+                            github="https://github.com/CALLmeDOMIN/summary-component"
+                            vercel="https://summarycomponent-dominiksieron.vercel.app/"
+                        />
+                        <FrontendMentor
+                            image="/previewcard.png"
+                            alt="preview card"
+                            github="https://github.com/CALLmeDOMIN/preview-card"
+                            vercel="https://previewcard-dominiksieron.vercel.app/"
+                        />
+                        <FrontendMentor
+                            image="/qrcode.png"
+                            alt="qrcode"
+                            github="https://github.com/CALLmeDOMIN/qrcode"
+                            vercel="https://qrcode-dominiksieron.vercel.app/"
+                        />
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default Mentor;
