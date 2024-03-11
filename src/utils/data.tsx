3913e24a91143a8de0fa471169fc1ef7f838f889
Dominik Sieroń <dominiksieron13@gmail.com>
2024-03-11 20:16:32 +0100

    Feat: Add uxberries

--- src/utils/data.tsx
+++ src/utils/data.tsx
@@ -85,16 +85,62 @@ export const experienceData: Experience[] = [
 ]
 
 export const projectData: Project[] = [
-    // {
-    //     title: "UXberries",
-    //     desc: "",
-    //     link: "",
-    //     githubLink: "https://github.com/CALLmeDOMIN/uxberries",
-    //     imageUrl: "/uxberries.png",
-    //     isWorkInProgress: false,
-    //     technologies: [],
-    //     align: "left",
-    // },
+    {
+        title: "UXberries",
+        desc: "The project focused on creating a scalable, maintainable codebase that would facilitate future enhancements and integration of new features. The result is a sophisticated platform that not only enhances the association's digital footprint but also serves as a vital resource for student collaboration and scientific discourse",
+        link: "https://uxberries.vercel.app",
+        githubLink: "https://github.com/CALLmeDOMIN/uxberries",
+        imageUrl: "/uxberries.png",
+        isWorkInProgress: false,
+        technologies: [
+            {
+                name: "Astro",
+                icon: (
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="24"
+                        height="24"
+                        viewBox="0 0 24 24"
+                        fill="none"
+                        stroke="currentColor"
+                        stroke-width="2"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        className="icon icon-tabler icons-tabler-outline icon-tabler-brand-astro"
+                        aria-label="Astro"
+                        id="Astro"
+                    >
+                        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                        <path d="M14.972 3.483c.163 .196 .247 .46 .413 .987l3.64 11.53a15.5 15.5 0 0 0 -4.352 -1.42l-2.37 -7.723a.31 .31 0 0 0 -.296 -.213a.31 .31 0 0 0 -.295 .214l-2.342 7.718a15.5 15.5 0 0 0 -4.37 1.422l3.657 -11.53c.168 -.527 .251 -.79 .415 -.986c.144 -.172 .331 -.306 .544 -.388c.242 -.094 .527 -.094 1.099 -.094h2.612c.572 0 .858 0 1.1 .094c.213 .082 .4 .217 .545 .39" />
+                        <path d="M9 18c0 1.5 2 3 3 4c1 -1 3 -3 3 -4q -3 1.5 -6 0" />
+                    </svg>
+                ),
+            },
+            {
+                name: "React",
+                icon: <IconBrandReact aria-label="React" id="React" />,
+            },
+            {
+                name: "TypeScript",
+                icon: (
+                    <IconBrandTypescript
+                        aria-label="TypeScript"
+                        id="TypeScript"
+                    />
+                ),
+            },
+            {
+                name: "Tailwindcss",
+                icon: (
+                    <IconBrandTailwind
+                        aria-label="Tailwindcss"
+                        id="Tailwindcss"
+                    />
+                ),
+            },
+        ],
+        align: "left",
+    },
     {
         title: "Car rental",
         desc: "Project is a car rental website. It symulates a car rental company. The intention is to learn web development, new technologies and build a fully functional site. It is a work in progress and will be updated with new features, since its purpose is to learn newest things in practice.",
@@ -134,7 +180,7 @@ export const projectData: Project[] = [
                 icon: <IconBrandPrisma aria-label="Prisma" id="Prisma" />,
             },
         ],
-        align: "left",
+        align: "right",
     },
     {
         title: "mytriproutes",
@@ -179,6 +225,6 @@ export const projectData: Project[] = [
                 icon: <IconBrandStripe aria-label="Stripe" id="Stripe" />,
             },
         ],
-        align: "right",
+        align: "left",
     },
 ]
